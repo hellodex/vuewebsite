e602f65b37fe12b04e46dd41d277623e34bcf539
huangfupo <1561117958@qq.com>
2025-03-18 17:29:18 +0800

    fix: 删除react-dom 验证代码

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,6 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['react-dom']
-  },
   server: {
     host: '0.0.0.0',
     port: 8001 // 将3000修改为你想要的端口号
