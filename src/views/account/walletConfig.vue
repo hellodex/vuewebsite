a7cc05b3be57e5538a36cc87c80d15b52abbfbae
huangfupo <1561117958@qq.com>
2025-03-19 17:48:48 +0800

    style: 钱包配置样式

--- src/views/account/walletConfig.vue
+++ src/views/account/walletConfig.vue
@@ -131,8 +131,8 @@ watch(customWalletInfo, (newValue) => {
   }
   .copy {
     margin-left: 4px;
-    width: 18px;
-    height: 18px;
+    width: 14px;
+    height: 14px;
     cursor: pointer;
   }
   .export-btn {
