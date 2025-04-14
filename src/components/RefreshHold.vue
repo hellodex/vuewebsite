a5ecacab88ca3a3ea9d2e5474c1f4b032e6bf7e7
huangfupo <1561117958@qq.com>
2025-04-14 14:53:34 +0800

    feat: 放开刷新仓位操作

---
+++ src/components/RefreshHold.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="refresh-btn" @click="handelRefresh">手动刷新仓位</span>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useGlobalStore } from '@/stores/global'
+import { APIrefreshhold } from '@/api'
+const globalStore = useGlobalStore()
+const customWalletInfo = computed(() => globalStore.customWalletInfo)
+const handelRefresh = async () => {
+  const res = await APIrefreshhold({
+    walletAddress: customWalletInfo.value.walletInfo?.wallet,
+    chainCode: customWalletInfo.value.walletInfo?.chainCode
+  })
+  console.log(res)
+}
+</script>
+
+<style lang="scss" scoped>
+.refresh-btn {
+  margin-right: 12px;
+  cursor: pointer;
+  color: var(--font-color-default) !important;
+  font-size: 12px !important;
+}
+</style>
