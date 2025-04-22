808a2773c5e56ba2ceb3173b93722c37a37b3fe4
huangfupo <1561117958@qq.com>
2025-04-22 09:39:17 +0800

    feat: 引入 echarts SVGRenderer 渲染器

--- src/utils/echarts.ts
+++ src/utils/echarts.ts
@@ -19,7 +19,7 @@ import {
 import { LabelLayout, UniversalTransition } from 'echarts/features'
 
 // 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
-import { CanvasRenderer } from 'echarts/renderers'
+import { CanvasRenderer, SVGRenderer } from 'echarts/renderers'
 
 // 注册必须的组件
 echarts.use([
@@ -33,6 +33,7 @@ echarts.use([
   LabelLayout,
   UniversalTransition,
   CanvasRenderer,
+  SVGRenderer,
   BarChart,
   LineChart,
   PieChart,
