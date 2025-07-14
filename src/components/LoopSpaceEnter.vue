d3b8536fc553defeef7a546a883c6b2201354910
suqinglong <suqinglong01@gmail.com>
2025-07-14 17:08:37 +0800

    loopspace link

--- src/components/LoopSpaceEnter.vue
+++ src/components/LoopSpaceEnter.vue
@@ -8,12 +8,11 @@
 import { APIGetLoopAccessToken } from '@/api/login';
 import { useGlobalStore } from '@/stores/global';
 import { computed, ref, watch } from 'vue';
-import { GetLoopAccessToken } from "@/api/login";
 
 const loopspaceLink = import.meta.env.VITE_LOOPSPACE_LINK
 const globalStore = useGlobalStore()
 const accessKey = ref("")
-const defaultAvatar = "https://firebasestorage.googleapis.com/v0/b/loop-prod/o/hq%2F8bc6c6da-76cc-4263-b7e8-e7bd61e09bfe.png?alt=media&token=e8b6d61e-3d2a-4501-836d-e91647b8e8f9"
+// const defaultAvatar = "https://firebasestorage.googleapis.com/v0/b/loop-prod/o/hq%2F8bc6c6da-76cc-4263-b7e8-e7bd61e09bfe.png?alt=media&token=e8b6d61e-3d2a-4501-836d-e91647b8e8f9"
 
 const requestAcessTokenByAccountInfo = async () => {
     const accountInfo = globalStore.accountInfo
@@ -22,13 +21,9 @@ const requestAcessTokenByAccountInfo = async () => {
         return
     }
     const uuid = accountInfo.uuid
-    const avatar = accountInfo.avatar || defaultAvatar
-    const nickname = accountInfo.nickname || ""
     if (uuid) {
-        const a = await APIGetLoopAccessToken({});
-        console.log("a", a)
-        const res = await GetLoopAccessToken(uuid, nickname, avatar)
-        accessKey.value = res
+        const res: any = await APIGetLoopAccessToken({});
+        accessKey.value = res.access_token
     }
 }
 
